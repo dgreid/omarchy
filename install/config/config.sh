2784d68bbea3a21f39f489d86a1a84b5ee182aa6
Dylan Reid <dgreid@dylanreid.com>
2025-11-09 00:43:11 -0800

    dgreid: symlink configs on install

--- install/config/config.sh
+++ install/config/config.sh
@@ -1,6 +1,9 @@
 # Copy over Omarchy configs
 mkdir -p ~/.config
 cp -R ~/.local/share/omarchy/config/* ~/.config/
+for d in ~/.local/share/omarchy/config/*; do
+  ln -s $d ~/.config
+done
 
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
