9a5ff5403de508190db70eafc768da23a341c96a
Dylan Reid <dgreid@dylanreid.com>
2025-11-09 17:12:53 -0800

    X.com apps use brave

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -8,7 +8,8 @@ omarchy-webapp-install "Google Maps" https://maps.google.com "Google Maps.png" "
 omarchy-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
 omarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png "omarchy-launch-webapp-chrome https://youtube.com"
 omarchy-webapp-install "GitHub" https://github.com/ GitHub.png
-omarchy-webapp-install "X" https://x.com/ X.png
+omarchy-webapp-install "X" https://x.com/ X.png "omarchy-launch-webapp-brave https://x.com"
+omarchy-webapp-install "Grok" https://grok.com/ X.png "omarchy-launch-webapp-brave https://grok.com"
 omarchy-webapp-install "Figma" https://figma.com/ Figma.png
 omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "omarchy-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
