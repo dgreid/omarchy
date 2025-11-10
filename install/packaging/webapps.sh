7f8977aadf301cd904225c2b04b8bb5f544d6848
Dylan Reid <dgreid@dylanreid.com>
2025-11-09 16:41:07 -0800

    gmail app with chrome

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -12,4 +12,4 @@ omarchy-webapp-install "X" https://x.com/ X.png
 omarchy-webapp-install "Figma" https://figma.com/ Figma.png
 omarchy-webapp-install "Discord" https://discord.com/channels/@me Discord.png
 omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "omarchy-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
-omarchy-webapp-install "Fizzy" https://app.fizzy.do/ Fizzy.png
+omarchy-webapp-install "Gmail" https://gmail.google.com Gmail.png "omarchy-launch-webapp-chrome https://gmail.google.com"
